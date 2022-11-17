ebd57018aaaf7bc07d7eec79a85c3b6aaeca683a
pushpinder13 <118421690+pushpinder13@users.noreply.github.com>
2022-11-17 11:57:33 +0530

    Create myFirstProgram.py

---
+++ myFirstProgram.py
@@ -0,0 +1,5 @@
+a="pushpinder"                   #1     
+def abc():                       #3
+    a="chitkara"                 #4
+    print("hello",a)             #5  
+abc()  #function calling         #2
